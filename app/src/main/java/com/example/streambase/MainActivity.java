b99fde4a331064ae95640b5b83c17f7392f44ce2
Aakash006 <aakashpatel141@gmail.com>
2020-10-31 20:26:55 -0400

    added search results list

--- app/src/main/java/com/example/streambase/MainActivity.java
+++ app/src/main/java/com/example/streambase/MainActivity.java
@@ -2,93 +2,27 @@ package com.example.streambase;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-import android.widget.EditText;
-import android.widget.TextView;
-import android.widget.Toast;
-
-import com.android.volley.AuthFailureError;
-import com.android.volley.Request;
-import com.android.volley.RequestQueue;
-import com.android.volley.Response;
-import com.android.volley.VolleyError;
-import com.android.volley.toolbox.StringRequest;
-import com.android.volley.toolbox.Volley;
-
-import org.json.JSONArray;
-import org.json.JSONException;
-import org.json.JSONObject;
-
-import java.util.HashMap;
-import java.util.Map;
 
 public class MainActivity extends AppCompatActivity {
-    EditText search;
-    Button searchBtn;
-    TextView service;
-    private RequestQueue queue;
+    Button goToSearchBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        search = (EditText) findViewById(R.id.search);
-        searchBtn = (Button) findViewById(R.id.searchBtn);
-        service = (TextView) findViewById(R.id.service);
-        queue = Volley.newRequestQueue(this);
+        goToSearchBtn = (Button) findViewById(R.id.searchPageBtn);
 
-        searchBtn.setOnClickListener(new View.OnClickListener() {
+        goToSearchBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                if (!search.getText().toString().equals("")) {
-                    StringRequest getMovie = searchNameStringRequest(search.getText().toString());
-                    queue.add(getMovie);
-                } else {
-                    Toast.makeText(MainActivity.this, "No movie specified", Toast.LENGTH_LONG).show();
-                }
-
+                Intent intent = new Intent(MainActivity.this, SearchActivity.class);
+                startActivity(intent);
             }
         });
-    }
-
-    private StringRequest searchNameStringRequest(String nameSearch) {
-        String url = "https://utelly-tv-shows-and-movies-availability-v1.p.rapidapi.com/lookup?term=" + nameSearch + "&country=ca";
-        return new StringRequest(Request.Method.GET, url,
-                new Response.Listener<String>() {
-                    // SUCCESS
-                    @Override
-                    public void onResponse(String response) {
-                        try {
-                            JSONObject result = new JSONObject(response);
-                            JSONArray jsonArray = result.getJSONArray("results");
-                            JSONObject info = jsonArray.getJSONObject(0);
-                            JSONArray locations = info.getJSONArray("locations");
-                            JSONObject first = locations.getJSONObject(0);
-                            String url = first.getString("display_name");
-
-                            service.setText(url);
-                        } catch (JSONException e) {
-                            Toast.makeText(MainActivity.this, e.getMessage(), Toast.LENGTH_LONG).show();
-                        }
-                    }
-                },
-                new Response.ErrorListener() {
-                    // ERROR
-                    @Override
-                    public void onErrorResponse(VolleyError error) {
-                        // display a simple message on the screen
-                        Toast.makeText(MainActivity.this, "Utelly is not responsing", Toast.LENGTH_LONG).show();
-                    }
-                }) {
-                @Override
-                public Map<String, String> getHeaders() throws AuthFailureError {
-                    Map<String, String>  headers = new HashMap<String, String>(
-
-                    return headers;
-                }
-        };
     }
 }
---
+++ app/src/main/java/com/example/streambase/SearchActivity.java
@@ -0,0 +1,102 @@
+package com.example.streambase;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+
+public class SearchActivity extends AppCompatActivity {
+    EditText search;
+    Button searchBtn;
+    ListView listOfResults;
+    private RequestQueue queue;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.search_activity);
+
+        search = (EditText) findViewById(R.id.search);
+        searchBtn = (Button) findViewById(R.id.searchBtn);
+        listOfResults = (ListView) findViewById(R.id.listOfResults);
+        queue = Volley.newRequestQueue(this);
+
+        searchBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (!search.getText().toString().equals("")) {
+                    StringRequest getMovie = searchNameStringRequest(search.getText().toString());
+                    queue.add(getMovie);
+                } else {
+                    Toast.makeText(SearchActivity.this, "No movie specified", Toast.LENGTH_LONG).show();
+                }
+
+            }
+        });
+    }
+
+    private StringRequest searchNameStringRequest(String nameSearch) {
+        String url = getString(R.string.url) + nameSearch + "&country=ca";
+        return new StringRequest(Request.Method.GET, url,
+                new Response.Listener<String>() {
+                    // SUCCESS
+                    @Override
+                    public void onResponse(String response) {
+                        try {
+                            JSONObject result = new JSONObject(response);
+                            JSONArray results = result.getJSONArray("results");
+                            ArrayList<String> moviesOrShows = new ArrayList<>();
+                            for (int i = 0; i < results.length(); i++) {
+                                JSONObject movieOrShow = results.getJSONObject(i);
+                                moviesOrShows.add(movieOrShow.getString("name"));
+                            }
+
+                            ListAdapter adapter = new ArrayAdapter<>(SearchActivity.this, android.R.layout.simple_list_item_1, moviesOrShows);
+                            listOfResults.setAdapter(adapter);
+
+                        } catch (JSONException e) {
+                            Toast.makeText(SearchActivity.this, e.getMessage(), Toast.LENGTH_LONG).show();
+                        }
+                    }
+                },
+                new Response.ErrorListener() {
+                    // ERROR
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        // display a simple message on the screen
+                        Toast.makeText(SearchActivity.this, "Utelly is not responding", Toast.LENGTH_LONG).show();
+                    }
+                }) {
+            @Override
+            public Map<String, String> getHeaders() throws AuthFailureError {
+                Map<String, String>  headers = new HashMap<String, String>();
+                headers.put("x-rapidapi-host", getString(R.string.host));
+                headers.put("x-rapidapi-key", getString(R.string.key));
+                return headers;
+            }
+        };
+    }
+}
